7f7990c45eb329d3c1cd4f5c1f03439f5e9b0455
kjs <k104agi@gmail.com>
2018-06-17 23:22:48 +0900

    두 번째 커밋 - ipynb 파일 이름 수정

--- crawler1.py
+++ crawler1.py
@@ -5,11 +5,10 @@ from bs4 import BeautifulSoup
 file_path = 'lol_list.html'
 url_list = 'http://www.leagueoflegends.co.kr/?m=news&cate=notice'
 
-
 if os.path.exists(file_path):
     html = open(file_path, 'rt').read()
 else:
-    response = requests.get(url_list, params)
+    response = requests.get(url_list)
     html = response.text
     open(file_path, 'wt').write(html)
 
@@ -17,4 +16,4 @@ soup = BeautifulSoup(html, 'lxml')
 all_title = soup.select('.contents-table .request-list td a')
 for title in all_title:
     print(title.text)
-
+    print()
--- crawler2.py
+++ crawler2.py
@@ -1,51 +1,36 @@
-import os
-from io import BytesIO
-from urllib import parse
 import requests
 from bs4 import BeautifulSoup
 
 
-# file_path = './lol_list.html'
-# url_list = 'http://www.leagueoflegends.co.kr/?m=news&cate=notice'
-# params = {
-#     'm': 'news',
-# }
-#
-# if os.path.exists(file_path):
-#     html = open(file_path, 'rt').read()
-# else:
-#     response = requests.get(url_list, params)
-#     html = response.text
-#     open(file_path, 'wt').write(html)
-#
-# soup = BeautifulSoup(html, 'lxml')
-# all_title = soup.select('.contents-table .request-list td a')
-# for title in all_title:
-#     print(title.text)
-
-
 class Notice:
     def __init__(self, title, date, view):
-        self.title = None
-        self.date = None
-        self.view = None
-
-    def crawl(self):
-        file_path = './lol_list.html'
-        url_list = 'http://www.leagueoflegends.co.kr/?m=news&cate=notice'
-
-        if os.path.exists(file_path):
-            html = open(file_path, 'rt').read()
-        else:
-            response = requests.get(url_list, params)
-            html = response.text
-            open(file_path, 'wt').write(html)
-
-        soup = BeautifulSoup(html, 'lxml')
-        all_title = soup.select('.request-list > tbody >tr')
-        title = all_title.text
-        print(title)
+        self.title = title
+        self.date = date
+        self.view = view
 
 
-if __name__ == '__main__':
-    crawl(self)
+url_list = 'http://www.leagueoflegends.co.kr/?m=news&cate=notice'
+response = requests.get(url_list)
+html = response.text
+
+soup = BeautifulSoup(html, 'lxml')
+all_info = soup.select('tbody > tr')
+
+# print(all_info)
+
+notice_list = list()
+
+for get_info in all_info:
+    title = get_info.select_one('td.tleft > a').text
+    date = get_info.select_one('td:nth-of-type(2)').text
+    view = get_info.select_one('td:nth-of-type(3)').text
+    #     print(title)
+    #     print(date)
+    #     print(view)
+    notice_list.append(Notice(title, date, view))
+
+for result in notice_list:
+    print(result.title)
+    print(result.date)
+    print(result.view)
+    print()
